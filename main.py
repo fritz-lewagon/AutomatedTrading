a203f312ffffbffd03fa79d016fb60e4785fba04
Friedrich Finken <66962420+fritz-lewagon@users.noreply.github.com>
2021-10-20 13:36:06 +0200

    Restructuring

--- main.py
+++ main.py
@@ -1,36 +1,98 @@
-from models.MarketData import MarketData
+from models.Order import Order
+from models.Instruments import Instruments
+from models.TradingVenue import TradingVenue
+from models.Token import Token
+import time
+import statistics
 
 
-def get_venues():
-    endpoint = f'venues/'
-    response = MarketData().get_response(endpoint)
-    print(response)
+def mean_reversion_decision(isin: str, x1: str = "d1"):
+    """
+    :param isin: pass the isin of your instrument
+    :param x1: pass what type of data you want to retrieve (m1, h1 or d1)
+    :return: returns whether you should buy (True) or sell (False), depending on MR criteria
+    """
+    market_data = Instruments(
+        isin=isin,
+        x1=x1
+    ).get_market_data()
+    d1_prices = market_data['results']
+    prices_close = [x["c"] for x in d1_prices]  # you can obviously change that to low, close or open
+    mean_price = statistics.mean(prices_close)
+    print(f'Mean Price: {mean_price}')
+    latest_close_price = Instruments(
+        isin=isin,
+        x1="m1"
+    ).get_latest_market_data()
+    print(f'Latest Close Price: {latest_close_price}')
+    if latest_close_price < mean_price:
+        return True
+    return False
 
 
-def get_instruments():
-    endpoint = f'instruments/?search=Tesla&tradable=True&type=stock'
-    response = MarketData().get_response(endpoint)
-    print(response)
+def check_if_buy(isin: str, x1: str = "d1"):
+    """
+    :param isin: pass the isin of the stock you are interested in
+    :param x1:  pass the market data format you are interested in (m1, h1, or d1)
+    """
+    # check for MR decision
+    if mean_reversion_decision(
+            isin=isin,
+            x1=x1
+    ):
+        # create a buy order if True is returned by MR decision function
+        try:
+            print('buy')
+            placed_order = Order(
+                isin=isin,
+                valid_until=time.time() + 86400,
+                side="buy",
+                quantity=1
+            ).place_order()
+            order_uuid = placed_order.get('uuid')
+            # subsequently activate the order
+            activated_order = Order().activate_order(order_uuid)
+            print(activated_order)
+            time.sleep(14400)  # check back in 4 hours
+        except Exception as e:
+            print(f'1{e}')
+            time.sleep(60)
+    else:
+        try:
+            # create a sell order if mean reversion decision returns False
+            print('sell')
+            placed_order = Order(
+                isin=isin,
+                valid_until=time.time() + 86400,
+                side="sell",
+                quantity=1
+            ).place_order()
+            order_uuid = placed_order.get('uuid')
+            activated_order = Order().activate_order(order_uuid)
+            print(activated_order)
+            time.sleep(14400)  # check back in 4 hours
+        except Exception as e:
+            print(f'2{e}')
+            time.sleep(60)
 
 
-def get_quotes():
-    endpoint = f'quotes/?isin=US19260Q1076'
-    response = MarketData().get_response(endpoint)
-    print(response)
+def mean_reversion():
+    """
+    main function to be executed
+    """
+    while True:
+        if TradingVenue().check_if_open():
+            # make sure that you always have a functioning token
+            Token().get_new_token()
+            # make buy or sell decision
+            check_if_buy(
+                isin="US88160R1014",  # this is Tesla, but you can obviously use any ISIN you like :)
+                x1="d1"
+            )
+        else:
+            # sleep until market reopens in case it is closed
+            time.sleep(TradingVenue().seconds_till_tv_opens())
 
 
-def get_ohlc():
-    endpoint = f'ohlc/d1/?isin=DE0008404005'
-    response = MarketData().get_response(endpoint)
-    print(response)
-
-
-def get_trades():
-    endpoint = f'trades/?isin=US19260Q1076'
-    response = MarketData().get_response(endpoint)
-    print(response)
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    get_trades()
+    mean_reversion()
